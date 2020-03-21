b3f76b04ca6f780601a452e429b54fb734102242
guohao <17688752511@163.com>
2020-03-22 00:07:02 +0800

    add: 增加useref处理文件，整理整个构建过程，形成完整版本

--- gulpfile.js
+++ gulpfile.js
@@ -164,7 +164,7 @@ exports.stream = done => {
 // }
 
 // 案例
-const { src, dest } = require('gulp')
+const { src, dest, watch } = require('gulp')
 const sass = require('gulp-sass')
 
 // css的处理
@@ -181,7 +181,8 @@ const style = () => {
     // sass转换后默认情况下结束的大括号}是放在样式最后一个属性的后面
     // 如果想要放在单独一行，在sass传入参数对象{outputStyle: 'expanded'}
     .pipe(sass({outputStyle: 'expanded'})) // 会将.scss转换会.css
-    .pipe(dest('dist'))
+    // .pipe(dest('dist'))
+    .pipe(dest('temp')) // 重构
 }
 
 // js的处理
@@ -198,7 +199,8 @@ const babel = require('gulp-babel')
 const script = () => {
   return src('src/assets/scripts/*.js', { base: 'src' })
     .pipe(babel({presets: ['@babel/preset-env']}))
-    .pipe(dest('dist'))
+    // .pipe(dest('dist'))
+    .pipe(dest('temp')) // 重构
 }
 
 // html的处理
@@ -207,7 +209,8 @@ const script = () => {
 const page = () => {
   return src('src/*.html', { base: 'src' })
     // .pipe() // 配置html转换插件
-    .pipe(dest('dist'))
+    // .pipe(dest('dist'))
+    .pipe(dest('temp')) // 重构
 }
 
 // 图片和字体文件的处理
@@ -244,23 +247,169 @@ const extra = () => {
 const del = require('del')
 
 const clean = () => {
-  return del('dist')
+  // return del('dist')
+  return del(['dist', 'temp']) // 重构
+}
+
+// 构建开发服务器，并热更新
+// 1 安装browser-sync
+// 2 引入const browserSync = require('browser-sync')
+const browserSync = require('browser-sync')
+// 3 创建一个开发服务器
+const bs = browserSync.create()
+// 4 建立一个gulp任务
+const serve = () => {
+  // 为了实现src下的内容改变，触发重新构建dist，我们要使用gulp的wath方法监听scr的文件变化
+  // 1 引入watch {watch} = require('gulp)
+  // 2 调用watch监听每一类文件的变化
+  watch('src/assets/styles/*.scss', style)
+  watch('src/assets/scripts/*.js', script)
+  watch('src/*.html', page)
+  // 开发阶段不需要编译这些文件
+  // watch('src/assets/images/**', image)
+  // watch('src/assets/fonts/**', image)
+  // watch('src/public/**', extra)
+  // 只需要监听这些文件发生变化时热更新即可
+  // 监听这些文件的变化，执行bs的reload方法
+  watch([
+    'src/assets/images/**',
+    'src/assets/fonts/**',
+    'src/public/**'
+  ], bs.reload)
+  
+  // 初始化开发服务器配置
+  bs.init({
+    notify: false, // 是否提示browser connected 
+    // port: 3001, // 配置启动端口号，默认3000
+    open: false, // 是否自动打开浏览器，默认true
+    // 配置哪些文件改变后自动更新
+    // 由于服务器运行的项目是基于dist的，所以src下的修改并不会导致浏览器内容自动更新
+    files: 'dist/**',
+    server: {
+      // 基础文件夹
+      // 可以传入一个数组，依次在对应的文件夹中寻找文件，这样在开发阶段就不需要将images、fonts等打包进dist
+      // baseDir: ['dist', 'src', 'public'],
+      baseDir: ['temp', 'src', 'public'], // 重构
+      // routes中的配置会优先于baseDir的配置
+      routes: {
+        '/node_modules': 'node_modules' // 如果是没有打包进dist的文件，可以通过这种方式访问到
+      }
+    }
+  })
+}
+
+const loadPlugins = require('gulp-load-plugins')
+const plugins = loadPlugins()
+
+// 使用useref处理文件路径转换
+const useref = () => {
+  // return src('dist/*.html', {base: 'dist'})
+  return src('temp/*.html', {base: 'temp'}) // 重构
+  // 通过searchPath配置用来查找文件的目录
+    // .pipe(plugins.useref({searchPath: ['dist', '.']}))
+    .pipe(plugins.useref({searchPath: ['temp', '.']})) // 重构
+    // 通过useref编译之后，可能会生成html、js、css三种类型的文件，可以对他们分别进行压缩混淆等操作
+    // 压缩html gulp-htmlmin
+    // 压缩js   gulp-uglify
+    // 压缩css  gulp-clean-css
+    // 由于这里是针对不同类型的文件进行操作，需要判断文件类型，使用gulp-if
+    // 1 安装 npm i --save-dev gulp-htmlmin gulp-uglify gulp-clean-css gulp-if
+    // 2 放入管道
+    // 注意：这里可能出现的问题是，src和dest操作的都是dist目录，也就是一边读一边写，可能会出现冲突
+    // 解决方法是将dest的目录修改一下： release
+    // 对js的处理
+    .pipe(plugins.if(/\.js$/, plugins.uglify()))
+    // 对css的处理，注意这里使用gulp-clean-css时，中划线会变成驼峰写法cleanCSS
+    .pipe(plugins.if(/\.css$/, plugins.cleanCss()))
+    // 对html的处理
+    // 默认情况下htmlmin只会将html标签中的空去去掉
+    // 如果要折叠空白行、内联样式、内联js，需要增加参数配置
+    // 其他配置根据需要查看htmlmin的配置
+    .pipe(plugins.if(/\.html$/, plugins.htmlmin({
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true
+    })))
+    // .pipe(dest('dist'))
+    // .pipe(dest('release'))
+    .pipe(dest('dist')) // 重构
 }
 
 // 多任务处理
 // html、css、js的处理互不干扰，所以可以使用parallel同时处理
-const compile = parallel(style, script, page, image, font)
+// const compile = parallel(style, script, page, image, font)
+// image,font在开发阶段不需要编译，放到build中
+const compile = parallel(style, script, page)
 
 // 1 将需要转义的和不需要的区分开
 // 2 使用series控制先删除，再编译
-const build = series(clean, parallel(compile, extra))
+// const build = series(clean, parallel(compile, extra))
+// 上线时要执行的任务
+// const build = series(clean, parallel(compile, image, font, extra))
+
+// compile之后要进行useref，需要串行执行
+const build = series(clean, parallel(series(compile, useref), image, font, extra)) // 重构
+
+// 开发阶段，只需要编译html、css、js，并监听文件变化进行热更新
+const develop = series(compile, serve)
+
 
 module.exports = {
+  clean,
   style,
   script,
   page,
   image,
   font,
   compile,
-  build
+  build,
+  serve,
+  develop,
+  useref
 }
+
+// 自动加载插件
+// 之前我们的插件都是通过安装，引入，传入pipe的方式使用的
+// 当插件较多时每一个都这么配置比较麻烦
+// 使用gulp-load-plugins可以自动加载对应的插件
+// 1 安装npm i --save-dev gulp-load-plugins
+// 2 引入
+// const loadPlugins = require('gulp-load-plugins')
+// const plugins = loadPlugins() // 执行该方法会返回一个对象，该对象中包含所有要使用的插件
+// 3 替换原来的插件
+// 例如将sass()替换为plugins.sass()即可
+
+// useref文件引用处理
+// 如果在开发阶段我们引用了node_modules下面的文件，在打包生产目录时是不会被打包进去的
+// 需要使用usefef进行处理
+// 1 安装
+// 2 在html添加特殊注释
+// <!-- build:css assets/styles/vendor.css -->
+// <link ref="stylesheet" href="node_modules/bootstrap/dist/bootstrap.css"></link>
+// <!-- endbuild -->
+// <!-- build:js assets/scripts/vendor.js -->
+// <script src="node_modules/bootstrap/dist/js/bootstrap.css"></script>
+// <!-- endbuild -->
+
+// 重新规划构建过程
+// 由于使用useref对dist修改，为了避免读写冲突而需要一个临时文件夹，需要对编译过程进行调整
+// src -> temp -> dist
+// 1 clean时增加clean temp文件夹
+// 2 将html、css、js的转换结果放入temp（image、font、extra不需要修改）
+// 3 serve的baseDir dist改为temp
+// 4 useref从temp取文件，放入dist
+
+
+// 整理要导出的任务
+// 1 要导出的：clean，build，develop
+// 2 其他的中间过程不需要导出
+// 3 可以将导出的任务配置到package.json中以便使用
+// "scripts": {
+//   "clean": "gulp clean",
+//   "build": "gulp build",
+//   "develop": "gulp develop"
+// }
+
+
+// 封装自动化构建工作流
+// gulp的构建过程完成后，为了可以在多个项目中快速使用
